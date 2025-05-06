3a220eae40f7e6cdf105a9b590f1402938fe1f5e
Kamille Rabelo <kamillersalgado@gmail.com>
2025-05-06 09:51:50 -0300

    Initialize Next.js project with TypeScript, create basic layout and page components

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const metadata = {
+    title: 'Tabnews',
+    description: 'Clone of Tabnews',
+}
+
+function RootLayout({
+    children,
+  }) {
+    return (
+        <html lang="en">
+        <body>{children}</body>
+        </html>
+    )
+}
+
+export default RootLayout
---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+function Page() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is a simple page</p>
+    </div>
+  );
+}
+
+export default Page;
