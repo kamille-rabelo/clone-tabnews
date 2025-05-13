4d2dca85bf1b2f074d3e5f6b7efb49688ec08805
Kamille Rabelo <166072938+kamille-rabelo@users.noreply.github.com>
2025-05-13 19:10:16 +0000

    add `database.js` and `.env`

---
+++ infra/database.js
@@ -0,0 +1,11 @@
+import { Client } from 'pg';
+
+async function query() {
+    const client = new Client({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB
+    })
+}
