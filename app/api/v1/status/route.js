626c167d159f37df475421b9e19a43fb22a152c0
Kamille Rabelo <166072938+kamille-rabelo@users.noreply.github.com>
2025-05-15 20:18:46 +0000

    update `route.js` to return database details

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,5 +1,27 @@
-export async function GET(request) {
+import database from "infra/database.js";
+
+export async function GET(req) {
+  const updatedAt = new Date().toISOString();
+  const queryResult = await database.query({
+    text: `
+    SELECT current_setting('server_version') AS server_version, 
+           current_setting('max_connections')::int AS max_connections,
+            count(*)::int AS opened_connections 
+            FROM pg_stat_activity 
+            WHERE datname = $1;
+  `,
+    values: [process.env.POSTGRES_DB],
+  });
+
   return Response.json({
-    message: "API is running",
+    status: "ok",
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: queryResult.rows[0].server_version,
+        max_connections: queryResult.rows[0].max_connections,
+        opened_connections: queryResult.rows[0].opened_connections,
+      },
+    },
   });
 }
--- tests/integration/app/api/v1/status/get.test.js
+++ tests/integration/app/api/v1/status/get.test.js
@@ -1,5 +1,11 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
 
+  const responseBody = await response.json();
+  const parsedUpdatedAt = new Date(responseBody.updated_at);
   expect(response.status).toBe(200);
+  expect(responseBody.updated_at).toBe(parsedUpdatedAt.toISOString());
+  expect(responseBody.dependencies.database.version).toEqual("17.0");
+  expect(responseBody.dependencies.database.max_connections).toBe(100);
+  expect(responseBody.dependencies.database.opened_connections).toBe(1);
 });
