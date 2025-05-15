119fa602a369c64251adf442719239f7437b108f
Kamille Rabelo <166072938+kamille-rabelo@users.noreply.github.com>
2025-05-15 20:17:31 +0000

    update `database.js` to handle errors better

--- infra/database.js
+++ infra/database.js
@@ -1,13 +1,25 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
-async function query() {
-    const client = new Client({
-        host: process.env.POSTGRES_HOST,
-        port: process.env.POSTGRES_PORT,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB
-    })
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+
+  try {
+    const res = await client.query(queryObject);
+    return res;
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
 }
 
-export default query;
+export default {
+  query,
+};
