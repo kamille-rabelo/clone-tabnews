fbb2559c8c278c8ca0cac3b4f3da9a7b9910286a
Kamille Rabelo <166072938+kamille-rabelo@users.noreply.github.com>
2025-05-28 19:07:21 +0000

    remove logging of PostgreSQL credentials in database connection

--- infra/database.js
+++ infra/database.js
@@ -10,13 +10,6 @@ async function query(queryObject) {
     ssl: process.env.NODE_ENV === "development" ? false : true,
   });
 
-  console.log("postgres credentials", {
-    host: client.host,
-    port: client.port,
-    user: client.user,
-    database: client.database,
-  });
-
   try {
     await client.connect();
     const res = await client.query(queryObject);
